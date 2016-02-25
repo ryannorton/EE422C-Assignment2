20ad843862ea23c06cb7564e474d29f3f32715fa
ryannorton <nort.ryan@gmail.com>
2016-02-24 18:45:55 -0600

    fix Electronics constructor

--- Assignment3/Electronics.java
+++ Assignment3/Electronics.java
@@ -2,6 +2,9 @@ package Assignment3;
 
 public class Electronics extends Item 
 {
+// instance variables
+   private boolean isFragile;  // fragile items require premium shipping
+
 // constructors
    /**
     * Create an Electronics item with initial parameters.
@@ -9,9 +12,24 @@ public class Electronics extends Item
     * @param unitPrice The base price per item.
     * @param quantity The number of items.
     * @param weight The weight of a single item in pounds.
+    * @param isFragile If the item is fragile or not.
+    * @param shippingState The state to ship the item to.
     */
-   Electronics(String name, float unitPrice, int quantity, int weight)
+   Electronics(String name, float unitPrice, int quantity, int weight, boolean isFragile, String shippingState)
    {
-      super(name, unitPrice, quantity, weight);
+      super(name, unitPrice, quantity, weight, shippingState);
+      this.isFragile = isFragile;
+   }
+   
+// override methods
+   /**
+    * Determines if an item requires premium shipping or not.
+    * Fragile electronics require premium shipping.
+    * @return true if an item requires premium shipping else false.
+    */
+   @Override
+   protected boolean requiresPremiumShipping()
+   {
+      return this.isFragile;
    }
 }
